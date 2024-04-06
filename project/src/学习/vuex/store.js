2371cfe5a14049633cd406920ef63dc72680515c
hutaifu <371452986@qq.com>
2024-04-06 20:16:59 +0800

    project

--- project/src/学习/vuex/store.js
+++ project/src/学习/vuex/store.js
@@ -1,9 +1,9 @@
+import Vue from "vue";
+import vuex, {mapMutations} from "vuex"
+
 const state = {
     sum:0,
 }
-
-
-
 const getters = {
     bigSum(state){
         return state * 10;
@@ -14,7 +14,37 @@ const getters = {
 
 import {mapState} from "vuex"
 import {mapGetters} from 'vuex'
+import {mapMutations} from 'vuex'
 
 const x = mapState({he:"sum"},{});
 const y = mapState(['sum',])
 let a = {...x};
+
+
+//模块化
+const config1 = {
+    namespaced:true,
+    actions:{},
+    mutations:{},
+    state:{},
+    getters:{}
+}
+const config2 = {
+    namespaced:true,
+    actions:{},
+    mutations:{},
+    state:{},
+    getters:{}
+}
+
+
+export default new Vuex.Store({
+    modules:{
+        a:config1,
+        b:config2,
+    }
+})
+
+mapMutations("a",{increament:"JIA"})
+this.$sotre.commit('a/ADD',"personObj")
+this.$sore.getters['a/bigNum']
